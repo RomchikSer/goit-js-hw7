a698e2ceabd2a246cc79c9c1f96252f2e2bf57b0
Roman Serdiuk <75645387+RomchikSer@users.noreply.github.com>
2021-05-26 16:04:47 +0200

    change

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -8,6 +8,10 @@ const counter = {
     }
 }
 
+// const decrementBtn = document.querySelector("[data-action=decrement]");
+// const incrementBtn = document.querySelector("[data-action=increment]");
+// const counterValue = document.querySelector("#value");
+
 const refs = {
     incrementBtn: document.querySelector("[data-action=increment]"),
     decrementBtn: document.querySelector("[data-action=decrement]"),
@@ -23,4 +27,3 @@ refs.decrementBtn.addEventListener("click", function () {
     counter.decrement();
     refs.counterValue.textContent = counter.value;
 });
-
